7a6ab6abad47f81afebd81d5b654ce0714c2e38c
Pechtnov <tonichok@gmail.com>
2022-09-01 14:25:12 +0300

    added a section with comments

--- src/js/script.js
+++ src/js/script.js
@@ -23,8 +23,20 @@ $(document).ready(function() {
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
         });
+        function changeItem (item) {
+            $(item).each(function(i) {
+                $(this).on('click', function(event) {
+                    event.preventDefault();
+                    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                })
+            })
+        }
+        
+        changeItem('.catalog-item__content')
+        changeItem('.catalog-item__list')
 
 })
 
