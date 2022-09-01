bc23ae041b468db051d366195f1e4e162dd92880
Pechtnov <tonichok@gmail.com>
2022-09-01 11:58:26 +0300

    added script tabs

--- src/js/script.js
+++ src/js/script.js
@@ -18,6 +18,16 @@
 //     );
 //   });
 
+$(document).ready(function() {
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+
+})
+
     let slider = tns({
         container: '.carousel__inner',
         items: 1,
@@ -36,3 +46,5 @@
     document.querySelector('.next').addEventListener('click', () => {
         slider.goTo('next')
     });
+
+
